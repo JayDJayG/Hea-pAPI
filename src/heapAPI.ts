d01a08e55152f5d367055f79c42518c5b053d828
mammadu <mammadudiallo@gmail.com>
2022-03-31 18:23:24 -0500

    Add test to init heapAPI object

--- src/heapAPI.ts
+++ src/heapAPI.ts
@@ -1,3 +1,11 @@
 export function sum(a: number, b: number): number {
     return a + b;
 }
+
+export class heap {
+    data: number[]
+
+    constructor() {
+        this.data = [];
+      }
+}
--- test/heapAPI.test.ts
+++ test/heapAPI.test.ts
@@ -1,9 +1,12 @@
 // const heap = require('../src/heapAPI.ts')
-import {sum} from '../src/heapAPI'
+import {sum, heap} from '../src/heapAPI'
 
 test('two plus two is four', () => {
     let value = sum(2, 2)
     expect(value).toBe(4);
   });
 
-  //
+test("init empty heap", () => {
+    const test_heap = new heap();
+    expect(test_heap.data.length).toBe(0)
+})
